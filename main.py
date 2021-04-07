0ed4bd3f27768f5143980662bb24d1b10d7ddcc8
Albert <xannanov.albert@mail.ru>
2021-04-07 12:37:08 +0600

    ready levels

--- main.py
+++ main.py
@@ -28,16 +28,30 @@ class Crawler:
         self.html_of_init_page = requests.get(url_init_page)
 
     def start(self):
+
         for page in self.pages:
-            if len(self.pages) > 4:
+            if len(self.pages) > 1000:
                 return True
             self.__get_all_links_from_page(page)
+        return False
+
+    def start_with_nesting(self):
+        counter = 0
+        for page in self.pages:
+            self.__get_all_links_from_page(page)
+            for sub_page in page.child_pages:
+                self.__get_all_links_from_page(sub_page)
+                # for sub_sub_page in sub_page.child_pages:
+                #     self.__get_all_links_from_page(sub_sub_page)
+            self.write_to_file()
+            counter += 1
+            print(counter)
         return False
 
     def write_to_file(self):
         with open("data.txt", "w", encoding="utf-8") as file:
             for page in self.pages:
-                file.write(str(page.url) + "\t" + page.get_str_children() + "\n")
+                file.write(page.url + "\nCHILDREN: " + page.get_str_children() + "\n\n\n ------------------------\n")
 
     def __get_all_links_from_page(self, page: Page):
         request = requests.get(page.url).text
@@ -58,9 +72,26 @@ class Crawler:
                     page.add_children(new_page)
                     self.pages.append(new_page)
 
+    def get_dict_from_pages(self):
+        result = {}
+        for page in self.pages:
+            if result.get(page.url) is None:
+                result.update(
+                    {
+                        page.url: list(
+                            map(lambda pg: pg.url, page.child_pages)
+                        )
+                    }
+                )
+        return result
+
 
 if __name__ == '__main__':
-    crawler = Crawler('https://keddr.com/novosti1/')
-    crawler.start()
+    crawler = Crawler('https://keddr.com/')
+    crawler.start_with_nesting()
 
     crawler.write_to_file()
+
+    data = crawler.get_dict_from_pages()
+
+    print(data)
---
+++ venv/test.py
@@ -0,0 +1,3 @@
+dictation = {"a": 1, "b": 2}
+
+print(dictation.get("s"))
